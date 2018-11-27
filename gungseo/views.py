b968ce185a80098bc4245752302c5db2243d7a79
김혜수 <hyesookim95@gmail.com>
2018-11-27 22:21:33 +0900

    결과보기 폰트 퍼센트 출력 및 50% 이상폰트만 출력

--- gungseo/views.py
+++ gungseo/views.py
@@ -43,9 +43,9 @@ def result(request):
 
 			#결과값 reshape 
 			#(ex) from [[0.1, 0.1, 0.1, 0.1, 0.1]] to [[0.1],[0.1],[0.1],[0.1],[0.1]]
-			analysis_result = analysis_result.reshape((8,1))
+			analysis_result = analysis_result.reshape((8))
 			#reshape한 결과값 리스트화
-			analysis_result = analysis_result.tolist(); 
+			analysis_result = [ round(i * 100, 2) for i in analysis_result.tolist()] 
 			font_name = ['궁서체' , '나눔손글씨붓', '맑은고딕', '바탕', '썸타임', '포천오성과한음', '한나는열한살', '훈화양연화']
 			#두 개의 리스트 각각 키와 밸류로 딕셔너리 만들기 => 폰트이름 , 확률값 쌍 생성
 			font_dict = dict(zip(font_name, analysis_result)) 
@@ -54,10 +54,8 @@ def result(request):
 			font_dict = sorted(font_dict.items(), key=lambda kv: kv[1], reverse=True)
 			#가능성이 가장 큰 탑 쓰리의 키값(폰트이름) 저장
 			top_three =[] 
-			for key, val in font_dict[:3]:
+			for key in font_dict[:3]:
 				top_three.append(key)
-				
-
 
 			#가장확률이 높은 클래스의 인덱스
 			# one_hot = analysis_result.argmax(axis=-1)	
